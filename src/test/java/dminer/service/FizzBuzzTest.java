dd7eda465f0bc704776175246d453f8bf785bdef
Nattapong Naoniavte <k.nattapong@gmail.com>
2018-02-20 12:26:34 +0700

    sample test FizzBuzz

--- src/test/java/dminer/service/FizzBuzzTest.java
+++ src/test/java/dminer/service/FizzBuzzTest.java
@@ -1,7 +1,9 @@
-package dminer.service;
+package test.java.dminer.service;
 
 import org.junit.Ignore;
 import org.junit.Test;
+
+import main.java.dminer.service.FizzBuzz;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
@@ -10,12 +12,20 @@ import static org.junit.Assert.assertThat;
 
 public class FizzBuzzTest {
     @Test
-    public void
-    getTokenNumberOneShouldSayOne() {
+    public void getTokenNumberOneShouldSayOne() {
         FizzBuzz fizzBuzz = new FizzBuzz();
         String expectedResult = "1";
 
         String actualResult = fizzBuzz.say(1);
+
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    public void getTokenNumberOneShouldSayFizzBuzz() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String expectedResult = "FizzBuzz";
+
+        String actualResult = fizzBuzz.say(15);
 
         assertEquals(expectedResult, actualResult);
     }
