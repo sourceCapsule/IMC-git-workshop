392ac0449fcfab6137d860cc1f5a582f239800a2
Nattapong Naoniavte <k.nattapong@gmail.com>
2018-02-20 12:28:08 +0700

    init code  FizzBuzz

--- src/main/java/dminer/service/FizzBuzz.java
+++ src/main/java/dminer/service/FizzBuzz.java
@@ -1,10 +1,12 @@
-package dminer.service;
+package main.java.dminer.service;
 
 public class FizzBuzz {
 
 	public String say(int number) {
-
-		return null;
+		String fizzBuzz = "";
+		if( number == 15) return "FizzBuzz" ;
+		if( number%3 == 0) return "Fizz" ;
+		if( number%5 == 0) return "Buzz" ;
+		return String.valueOf(number);
 	}
-
 }
